cf23e49f877d335fac9c931781f470a4f75f55f1
lsan <1124578298@qq,com>
2020-10-11 20:28:51 +0800

    add all

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList res = A;
+        while (A.rest != null){
+            A = A.rest;
+        }
+       A.rest = B;
+        return res;
     }
 
     /**
@@ -91,7 +96,20 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList res = new IntList(A.first, null);
+        IntList tem = res;
+        A = A.rest;
+        while (A != null){
+            tem.rest = new IntList(A.first, null);
+            tem = tem.rest;
+            A = A.rest;
+        }
+        while (B != null){
+            tem.rest = new IntList(B.first, null);
+            tem = tem.rest;
+            B = B.rest;
+        }
+        return res;
     }
 
 
